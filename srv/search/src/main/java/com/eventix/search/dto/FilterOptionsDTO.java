a1fd8838665f1e1080e04010f36c48f1fa8ba476
Annu Kumari <annu.kumari02@sap.com>
2025-11-04 11:57:18 +0530

    FilterOptionsDTO updated

--- srv/search/src/main/java/com/eventix/search/dto/FilterOptionsDTO.java
+++ srv/search/src/main/java/com/eventix/search/dto/FilterOptionsDTO.java
@@ -1,11 +1,15 @@
 package com.eventix.search.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class FilterOptionsDTO {
     private List<String> categories;
     private List<String> cities;
-    private List<String> tags;
 }
