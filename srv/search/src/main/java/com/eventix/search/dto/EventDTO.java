0eb2e9cf401e4fb954e70151cf1de17d320a8fac
Annu Kumari <annu.kumari02@sap.com>
2025-11-04 11:56:43 +0530

    EventDTO updated

--- srv/search/src/main/java/com/eventix/search/dto/EventDTO.java
+++ srv/search/src/main/java/com/eventix/search/dto/EventDTO.java
@@ -1,9 +1,14 @@
 package com.eventix.search.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class EventDTO {
     private String eventId;
     private String title;
