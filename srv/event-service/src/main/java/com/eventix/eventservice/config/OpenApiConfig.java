c065696010ab093a1a109cad4076e3385fab5278
Abhishek Kumar <ak00873@gmail.com>
2025-11-05 16:13:31 +0530

    feat: add Swagger

---
+++ srv/event-service/src/main/java/com/eventix/eventservice/config/OpenApiConfig.java
@@ -0,0 +1,34 @@
+package com.eventix.eventservice.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Event Management Service API")
+                        .version("1.0.0")
+                        .description("Microservice responsible for creating, managing, and discovering community events for the Eventix platform.")
+                        .contact(new Contact()
+                                .name("Eventix Dev Team")
+                                .email("support@eventix.io")
+                                .url("https://eventix.io"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0")))
+                .servers(List.of(
+                        new Server().url("http://localhost:8080").description("Local Dev Server")
+                ));
+    }
+}
