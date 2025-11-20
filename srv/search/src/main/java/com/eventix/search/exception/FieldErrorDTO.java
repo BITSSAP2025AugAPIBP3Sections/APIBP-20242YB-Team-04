8cac86b0d040c88fa2b8586f4dc92f9465dce8c5
Annu Kumari <annu.kumari02@sap.com>
2025-11-21 00:57:33 +0530

    updated error handling

---
+++ srv/search/src/main/java/com/eventix/search/exception/FieldErrorDTO.java
@@ -0,0 +1,11 @@
+package com.eventix.search.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class FieldErrorDTO {
+    private String field;
+    private String message;
+}
