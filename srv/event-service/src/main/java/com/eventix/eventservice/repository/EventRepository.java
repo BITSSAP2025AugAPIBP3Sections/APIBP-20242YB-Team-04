4057982a414470019ed36adff208947a2f792a01
Saloni <salonilokhande2003@gmail.com>
2025-11-25 13:22:45 +0530

    Resolved build issues for event-service and updated port

--- srv/event-service/src/main/java/com/eventix/eventservice/repository/EventRepository.java
+++ srv/event-service/src/main/java/com/eventix/eventservice/repository/EventRepository.java
@@ -1,11 +1,15 @@
 package com.eventix.eventservice.repository;
 
 import com.eventix.eventservice.model.Event;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.UUID;
 
 public interface EventRepository extends JpaRepository<Event, UUID>, JpaSpecificationExecutor<Event> {
+    
+    Page<Event> findByOrganizerId(String organizerId, Pageable pageable);
 
 }
